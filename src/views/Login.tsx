698701781468c6ebc3e26507029a575c9b9a3490
kacto1973 <a222212359@unison.mx>
2024-10-15 15:23:27 -0700

    ajuste del view login, modificaciones en app e index css, cambio de imagen de peines a una con más resolución

--- src/views/Login.tsx
+++ src/views/Login.tsx
@@ -30,7 +30,7 @@ function LoginForm() {
 
   const credentials = {
     email: email,
-    password: password
+    password: password,
   };
 
   const mutation = useMutation({
@@ -40,17 +40,20 @@ function LoginForm() {
       queryClient.invalidateQueries({ queryKey: ["userInfo"] });
     },
   });
-    if(mutation.isSuccess){
-      if( mutation.data.success){
-        localStorage.setItem('success',mutation.data.success+'')
-        navigate("/dashboard");
-      }
-
+  if (mutation.isSuccess) {
+    if (mutation.data.success) {
+      localStorage.setItem("success", mutation.data.success + "");
+      navigate("/dashboard");
     }
+  }
   return (
     <>
-      <div className="bg-[#353232] overflow-hidden">
+      {/*bg gris , div padre*/}
+      <div className="bg-[#353232]">
+        {/*divide en dos columnas la pagina, una pa la foto y otra para el login*/}
         <div className="grid sm:grid-cols-2">
+          {/*este div es del lado izquierdo*/}
+          {/*pone encima uno del otro  la imagen del logo y el div blanco */}
           <div className="flex flex-col">
             <div className="w-full flex justify-center items-center mt-5">
               <img
@@ -59,7 +62,9 @@ function LoginForm() {
                 alt="Imagen del logo y titulo"
               />
             </div>
+            {/*fin del div que contien el logo*/}
 
+            {/*div del cuadro blanco*/}
             <div className="w-3/5 h-4/6 mt-10 pt-10 mx-auto bg-white rounded-md">
               <div className="p-8 mx-8 mb-8">
                 <h1 className="mb-10 md:text-4xl sm:text-3xl text-xl text-[#E90074] whitespace-nowrap">
@@ -82,24 +87,27 @@ function LoginForm() {
                     text={"Iniciar sesión"}
                     type={"submit"}
                     onClick={(event) => {
-                        event.preventDefault();
-                        mutation.mutate(credentials);
+                      event.preventDefault();
+                      mutation.mutate(credentials);
                     }}
                   />
                   <a
                     className="flex justify-center sm:text-2xl text-lg whitespace-nowrap p-auto py-5 hover:underline"
-                    href="/templates/loginAdmin.html">
+                    href="/templates/loginAdmin.html"
+                  >
                     Registrar nuevo usuario
                   </a>
                 </form>
               </div>
             </div>
+            {/*fin del cuadro blanco*/}
           </div>
+          {/*fin del div del lado izuqierdo*/}
 
           <div className="bg-black sm:block">
             <img
-              src="src/images/peines.jpeg"
-              className="h-screen w-full object-cover opacity-50 "
+              src="src/images/peines.webp"
+              className="h-full object-cover opacity-50"
               alt="Imagen grandota"
             />
           </div>
