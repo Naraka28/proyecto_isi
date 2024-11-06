e6f00671cd82859495d24aa53c1cf94f783f0d56
Gael Borchardt <gaelborchardt@gmail.com>
2024-11-06 10:49:01 -0700

    colorsitos

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 export default {
   content: ["./index.html","./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primaryBlack: '#1E2328',
+        secondaryBlack: '#3F434B',
+        tertiaryBlack: '#575D64',
+        primaryYellow: '#F5A302',
+        secondaryYellow: '#F4B301',
+        tertiaryYellow: '#FCDE70',
+      },
+    },
   },
   plugins: [],
 }
